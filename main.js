a75d662d3f4543a736eb3103111cd67d81135f98
Chris <christopher.tan17@gmail.com>
2017-04-11 01:54:43 -0700

    Fixed Navbar

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
+  $(window).scroll(function(){
+    var height = $('.header').height();
+    if($(this).scrollTop() >= height){
+      $(".fixed").addClass("fixed-top");
+      $(".body > .content").addClass("fixed-bottom");
+    }
+    else {
+      $(".fixed").removeClass("fixed-top");
+      $(".body > .content").removeClass("fixed-bottom");
+    }
+  });
   $(".toggle-collapse").click(function(){
-    $("#collapse").slideToggle(800);
     $(".fixed li").css("display", "block");
+    $("#collapse").slideToggle(800);
   });
 });
