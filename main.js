048811759186537a647b3050dcfc0000b0ebb9c8
Chris <christopher.tan17@gmail.com>
2017-06-30 18:44:22 -0700

    Menu Changes

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  var collapsed = true;
+  $("#menu").click(function(){
+    if(collapsed == true){
+      $("#menu-collapse").css("display", "block");
+      collapsed = false;
+    }
+    else {
+      $("#menu-collapse").css("display", "none");
+      collapsed = true;
+    }
+  });
 });
