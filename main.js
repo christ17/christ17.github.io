348eaa44690da6970dcf45bf577950cf1c014878
Christopher Tan <chris@Christophers-MacBook-Pro.local>
2018-01-03 01:00:13 -0800

    Peerlift site

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
+var slideIndex = 1;
 $(document).ready(function(){
-  $('#diseases-dropdown').click(function(){
-    menuResolveConflict('diseases','about');
-  });
-  $('#about-dropdown').click(function(){
-    menuResolveConflict('about','diseases');
-  });
-  function menuResolveConflict(id1, id2) {
-    $('#'+id1+'-dropdown').toggleClass('active-menu');
-    if($('#'+id2+'-dropdown').hasClass('active-menu')){
-      $('#'+id2+'-dropdown').removeClass('active-menu');
+  $(window).scroll(function(){
+    var height = $("[class$='header-section']").height();
+    if($(this).scrollTop() >= height){
+      $("#navbar").css("position", "fixed");
+      $('a[href="#donate"]').css({"position": "fixed", "top": "67vh"});
+      $(".body").css("margin-top", "100px");
     }
-    $('#'+id1+'-menu').toggleClass('display');
-    if($('#'+id2+'-menu').hasClass('display')){
-      $('#'+id2+'-menu').removeClass('display');
+    else {
+      $("#navbar").css("position", "relative");
+      $('a[href="#donate"]').css({"position": "absolute", "top": "167vh"});
+      $(".body").css("margin-top", "0px");
     }
-  }
+    if($("#opportunities-menu").css("display") == "block"){
+      var margin = parseInt($(".body").css("margin-top"));
+      margin += $("#opportunities-menu").outerHeight();
+      $(".body").css("margin-top", margin+"px");
+    }
+  });
+  $('a[href^="#"]').click(function(event) {
+    var target = $(this.getAttribute('href'));
+    event.preventDefault();
+    $('html, body').animate({scrollTop: target.offset().top - 100}, 800);
+  });
+  $("#menu-button").click(function(){
+    if($("#menu-button > i").hasClass("fa-bars")) {
+      toggleMenu("fa-bars", "fa-times");
+    }
+    else {
+      toggleMenu("fa-times", "fa-bars");
+    }
+  });
+  $("#opportunities-dropdown").click(function(){
+    var margin = parseInt($(".body").css("margin-top"));
+    if($("#opportunities-menu").css("display") == "none"){
+      $("#opportunities-menu").css("display", "block");
+      margin += $("#opportunities-menu").outerHeight();
+    }
+    else {
+      $("#opportunities-menu").css("display", "none");
+      margin -= $("#opportunities-menu").outerHeight();
+    }
+    $(".body").css("margin-top", margin+"px");
+  });
+  $("#slide-left").click(function(){
+    slider(-1);
+  });
+  $("#slide-right").click(function(){
+    slider(1);
+  });
 });
+function toggleMenu(current, desired) {
+  $("#menu-button > i").removeClass(current);
+  $("#menu-button > i").addClass(desired);
+  $("#navbar > .container > nav"). toggleClass("display-menu");
+};
+function slider(value) {
+  $("#mission > div:nth-child(" + (2 + slideIndex) + ")").css("display", "none");
+  slideIndex += value;
+  if(slideIndex == 0) slideIndex = 4;
+  else if(slideIndex == 5) slideIndex = 1;
+  $("#mission > div:nth-child(" + (2 + slideIndex) + ")").css("display", "block");
+}
