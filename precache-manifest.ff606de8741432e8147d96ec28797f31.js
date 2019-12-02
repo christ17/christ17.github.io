2d6e35cc5eb6de4325a03ad2604da379f81460ec
Christopher Tan <christopher.tan17@gmail.com>
2019-12-02 11:47:51 -0800

    EJ

---
+++ precache-manifest.ff606de8741432e8147d96ec28797f31.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0692c9b5bae78bd1c7e29c67aac0ce06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb973d9a8b34aab5990",
+    "url": "/static/css/main.8dda5228.chunk.css"
+  },
+  {
+    "revision": "bf838253568ec9f94ba7",
+    "url": "/static/js/2.e7e4a9e9.chunk.js"
+  },
+  {
+    "revision": "eeb973d9a8b34aab5990",
+    "url": "/static/js/main.9d6172c1.chunk.js"
+  },
+  {
+    "revision": "402f6765785b6c5b5052",
+    "url": "/static/js/runtime-main.cb1e73e2.js"
+  },
+  {
+    "revision": "6dbed8ddbd64e6935a306e61e08a2672",
+    "url": "/static/media/1619.6dbed8dd.jpg"
+  },
+  {
+    "revision": "d406a484141a61c92eb98631499fe284",
+    "url": "/static/media/aemp.d406a484.png"
+  },
+  {
+    "revision": "1ac8cb5c2826b25edb4507f625d19baf",
+    "url": "/static/media/ajl.1ac8cb5c.png"
+  },
+  {
+    "revision": "007a0229643ccec729c3ff922185b1ef",
+    "url": "/static/media/chinese.007a0229.jpg"
+  },
+  {
+    "revision": "4f7c8ba5450a3c4658b44eb77e40dd72",
+    "url": "/static/media/cobalt.4f7c8ba5.jpg"
+  },
+  {
+    "revision": "da3ca30d25e11a4c96f8d1a1180a4b51",
+    "url": "/static/media/corrina.da3ca30d.mp3"
+  },
+  {
+    "revision": "d83797516ef0a847c869b7a42fbfa19a",
+    "url": "/static/media/lennar.d8379751.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "aae55e9a1f7c7d011b091728b9aa14ed",
+    "url": "/static/media/ohlonemap.aae55e9a.jpg"
+  },
+  {
+    "revision": "7d8ecee07cf497b54eb0b392ce0b70d9",
+    "url": "/static/media/redlining.7d8ecee0.png"
+  },
+  {
+    "revision": "35e75af8b13238c86bb574ef49de9a52",
+    "url": "/static/media/redlininggentrification.35e75af8.gif"
+  },
+  {
+    "revision": "568c6c66e8243549512a80a9dac18523",
+    "url": "/static/media/shellmound.568c6c66.jpg"
+  }
+]);
